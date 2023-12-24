eaad4212ac17ef16dbc6a2112b305fccce124c49
vitorbelarmino <vitor.belarmino@hotmail.com>
2023-12-24 19:07:03 -0300

    fix: userApi refactor for better error handling

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,18 +1,34 @@
+import { IRegister } from '@/interface/IUser'
 import { api } from '.'
 import { AxiosError } from 'axios'
 
-export async function login (email: string, password: string): Promise<string> {
+export async function login (email: string, password: string): Promise<string | undefined> {
   try {
     const { data } = await api.post('/login', { email, password })
-    console.log(data)
-
     return data.token
   } catch (error) {
     if (error instanceof AxiosError) {
-      if (error.response?.status === 401) {
-        return 'Email ou senha inválidos'
+      if (error.response?.status === 401 || error.response?.status === 404) {
+        alert('Email ou senha inválidos')
+        return
       }
     }
-    return 'Error interno no servidor, aguarde alguns instantes e tente novamente'
+    alert('Error interno no servidor, aguarde alguns instantes e tente novamente')
+  }
+}
+
+export async function register (registerInfo: IRegister): Promise<string | undefined> {
+  const { name, email, password } = registerInfo
+  try {
+    const { data } = await api.post('/register', { name, email, password })
+    return data.token
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 409) {
+        alert('Email já cadastrado')
+        return
+      }
+    }
+    alert('Error interno no servidor, aguarde alguns instantes e tente novamente')
   }
 }
